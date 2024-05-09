0108a33cd7b7aa85159a9e1a928eb401d16d89e2
Amisha Naik <132745732+amishana1k@users.noreply.github.com>
2024-05-09 11:45:00 +0530

    Time: 41 ms (77.22%) | Memory: 17.1 MB (47.43%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        a = ""
+        for x in s:
+            if x.isalpha(): a += x.lower()
+            if x.isnumeric(): a += x
+        return a == a[::-1]
+        
